537b26f92f687d86d1e8a9432c1eb2a22373d1b7
White Hand <White Hand@DESKTOP-8O2Q7AJ>
2022-12-14 02:13:58 +0300

    Add ReadFile

---
+++ GeneratorLib/Generator.cs
@@ -0,0 +1,18 @@
+﻿namespace GeneratorLib
+{
+    public class Generator
+    {
+        private async Task<FileInfo> ReadFiles(string sourceFile)
+        {
+            Console.WriteLine("Reading.....");
+            string content;
+            using (var reader = new StreamReader(new FileStream(sourceFile, FileMode.Open)))
+            {
+                content = await reader.ReadToEndAsync();
+            }
+            return new FileInfo(sourceFile, content);
+        }
+
+
+    }
+}
